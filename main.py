2fe608873c6bcc4526367d253591c145cf6d93b6
PersimMonMon <buitho@oregonstate.edu>
2025-12-01 18:53:11 -0800

    create serialize string microservice

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel 
+from typing import Dict 
+
+app = FastAPI() 
+
+def escape_string(string: str) -> str:
+    return string.replace('"', '\\"')
+
+def serialize_string(data: Dict[str, str]) -> str:
+    compress_dict = []
+
+    # go over each tuple and append to new dict
+    for key, value in data.items():
+        key_str = f"\"{escape_string(key)}\""
+        val_str = f"\"{escape_string(value)}\""
+        compress_dict.append(f"{key_str}:{val_str}")
+    
+    # return in dict form (as string) 
+    return "{" + ",".join(compress_dict) + "}"
+
+class SerializeRequest(BaseModel):
+    data: Dict[str, str] # use Dict to enforce string input and return 
+
+@app.post("/serialize")
+def serialize(req: SerializeRequest):
+    serialize = serialize_string(req.data)
+    return serialize
